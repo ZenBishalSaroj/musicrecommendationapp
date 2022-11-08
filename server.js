b3d30faf51f5b7d0e252dfb26025e085cdf43894
benbanstola <66547759+benbanstola@users.noreply.github.com>
2022-11-07 22:49:57 -0500

    auth

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import notFoundMiddleware from './middleware/not-found';
 import dotenv from 'dotenv';
 import connectDB from './db/connect.js'
+import authRouter from './routes/authRoutes';
 dotenv.config()
 
 const app=express()
@@ -12,6 +13,10 @@ app.get('/',(req,res)=>{
 app.use(notFoundMiddleware)
 const port=process.env.port || 500
 app.listen(port,()=>console.log(`Server is runnning on port ${port}`))
+
+app.use(express.json())
+
+app.use('api/v1/auth',authRouter)
 
 const start=async()=>{
     try {
